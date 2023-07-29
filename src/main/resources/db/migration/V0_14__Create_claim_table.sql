bd008d50b5e079dae743416b43a76a8afe325b32
NiceGuyMe <loica.etudiant@gmail.com>
2023-07-29 15:52:22 +0300

    feat(migration): create claim migration

---
+++ src/main/resources/db/migration/V0_14__Create_claim_table.sql
@@ -0,0 +1,20 @@
+do $$
+    begin
+        if not exists(select from pg_type where typname = 'claim_status') then
+            create type "claim_status" as enum ('OPEN', 'CLOSE');
+        end if;
+    end $$;
+
+create table if not exists "claim"
+(
+    id                varchar
+        constraint claim_pk primary key default uuid_generate_v4(),
+    version_id        varchar,
+    transcript_id     varchar,
+    reason            varchar,
+    status            claim_status             not null,
+    closed_datetime   timestamp with time zone,
+    creation_datetime timestamp with time zone
+);
+
+create index if not exists claim_status_index on "claim" (status);
