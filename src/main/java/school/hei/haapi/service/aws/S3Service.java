b6220d9ad3521fed50a4ab2db7c195ec9a68906a
NiceGuyMe <loica.etudiant@gmail.com>
2023-08-03 09:19:39 +0300

    chore: update S3 service

--- src/main/java/school/hei/haapi/service/aws/S3Service.java
+++ src/main/java/school/hei/haapi/service/aws/S3Service.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import school.hei.haapi.endpoint.event.EventConf;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
@@ -14,7 +15,7 @@ import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 @Service
 @AllArgsConstructor
 public class S3Service {
-  private final S3Client s3Client;
+  private final EventConf eventConf;
 
   // Méthode pour télécharger un fichier depuis S3
   public byte[] downloadFile(String bucketName, String key) throws IOException {
@@ -23,30 +24,27 @@ public class S3Service {
         .key(key)
         .build();
 
-    return s3Client.getObjectAsBytes(getObjectRequest).asByteArray();
+    return eventConf.getS3Client().getObjectAsBytes(getObjectRequest).asByteArray();
   }
-
   // Méthode pour uploader un fichier vers S3
   public void uploadFile(String bucketName, String key, MultipartFile file) throws IOException {
     PutObjectRequest putObjectRequest = PutObjectRequest.builder()
         .bucket(bucketName)
         .key(key)
         .build();
-
-    s3Client.putObject(putObjectRequest, RequestBody.fromBytes(file.getBytes()));
+    eventConf.getS3Client().putObject(putObjectRequest, RequestBody.fromBytes(file.getBytes()));
   }
-
   // Méthode pour modifier un fichier existant dans S3
   public void modifyFile(String bucketName, String key, MultipartFile file) throws IOException {
     // Vérifier si le fichier existe déjà
-    if (s3Client.headObject(headRequest -> headRequest.bucket(bucketName).key(key)).hasMetadata()) {
+    if (eventConf.getS3Client().headObject(headRequest -> headRequest.bucket(bucketName).key(key)).hasMetadata()) {
       // Mettre à jour le contenu du fichier
       PutObjectRequest putObjectRequest = PutObjectRequest.builder()
           .bucket(bucketName)
           .key(key)
           .build();
 
-      s3Client.putObject(putObjectRequest, RequestBody.fromBytes(file.getBytes()));
+      eventConf.getS3Client().putObject(putObjectRequest, RequestBody.fromBytes(file.getBytes()));
     } else {
       // Si le fichier n'existe pas, on l'upload simplement
       uploadFile(bucketName, key, file);
