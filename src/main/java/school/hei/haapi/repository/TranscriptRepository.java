da712b9921ccafa03a9867acf0d8551962751800
Ranomenjanahary <hei.jaonary@gmail.com>
2023-08-03 07:49:28 +0300

    chore: change import

--- src/main/java/school/hei/haapi/repository/TranscriptRepository.java
+++ src/main/java/school/hei/haapi/repository/TranscriptRepository.java
@@ -6,18 +6,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import school.hei.haapi.endpoint.rest.model.Transcript;
+import school.hei.haapi.model.Transcript;
 
 @Repository
-public interface TranscriptRepository extends JpaRepository<Transcript, String> {
+public interface TranscriptRepository extends JpaRepository<school.hei.haapi.model.Transcript, String> {
   @Query(value = "SELECT t FROM Transcript t WHERE t.id = :transcript_id"
       + " AND t.student.id = :student_id")
-  Transcript getTranscriptByIdAndStudentId(
+  school.hei.haapi.model.Transcript getTranscriptByIdAndStudentId(
       @Param("transcript_id") String transcriptId,
       @Param("student_id") String studentId);
 
   @Query(value = "SELECT t FROM Transcript t WHERE t.student.id = :student_id")
-  List<Transcript> getAllTranscriptsByStudentId(
+  List<school.hei.haapi.model.Transcript> getAllTranscriptsByStudentId(
       @Param("student_id") String studentId, Pageable pageable);
 
   List<Transcript> getAllByStudentId(String studentId);
