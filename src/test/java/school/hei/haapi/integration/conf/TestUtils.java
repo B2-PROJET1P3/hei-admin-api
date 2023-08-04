9c418045de7b917925d4af1676514ff66841d365
Ranomenjanahary <hei.jaonary@gmail.com>
2023-08-04 07:17:34 +0300

    feat: add transcript integration test

--- src/test/java/school/hei/haapi/integration/conf/TestUtils.java
+++ src/test/java/school/hei/haapi/integration/conf/TestUtils.java
@@ -56,6 +56,11 @@ public class TestUtils {
   public static final String COURSE3_ID = "course3_id";
   public static final String COURSE4_ID = "course4_id";
   public static final String COURSE5_ID = "course5_id";
+  public static final String TRANSCRIPT1_ID = "transcript1_id";
+  public static final String TRANSCRIPT2_ID = "transcript2_id";
+  public static final String TRANSCRIPT4_ID = "transcript4_id";
+  public static final String NEW_TRANSCRIPT_ID = "new_transcript_id";
+  public static final String NON_EXISTANT_STUDENT_ID = "non_existant_student_id";
 
   public static final String BAD_TOKEN = "bad_token";
   public static final String STUDENT1_TOKEN = "student1_token";
