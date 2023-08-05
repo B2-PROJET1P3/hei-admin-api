f2769ec7f30052826ec67fc5fbb3492e2500a0d2
Jonatana RANDRIAMPARANY Andrianina <Jonas4884>
2023-08-05 13:22:47 +0300

    chore(migration): update version

--- src/main/resources/db/migration/V0_16__Create_version_table.sql
+++ src/main/resources/db/migration/V0_16__Create_version_table.sql
@@ -1,14 +1,23 @@
+do
+$$
+    begin
+        if not exists(select from pg_type where typname = 'role') then
+            create type "role" as enum ('STUDENT', 'TEACHER', 'MANAGER');
+        end if;
+    end
+$$;
+
 create table if not exists "version"(
-                             id  varchar
-                                 constraint version_pk primary key default uuid_generate_v4(),
-                             transcript_id  varchar                  not null
-                                 constraint transcript_id_fk references "transcript"(id),
-                             ref int,
-                             created_by_user_id  varchar                  not null
-                                 constraint user_id_fk references "user"(id),
-                             created_by_user_role  varchar                  not null
-                                 constraint user_role_id_fk references "user"(role),
-                             creation_datetime timestamp without time zone not null default now()
+                                        id  varchar
+                                            constraint version_pk primary key default uuid_generate_v4(),
+                                        transcript_id  varchar                  not null
+                                            constraint transcript_id_fk references "transcript"(id),
+                                        ref int,
+                                        created_by_user_id  varchar                  not null
+                                            constraint user_id_fk references "user"(id),
+                                        created_by_user_role  role                  not null,
+
+                                        creation_datetime timestamp without time zone not null default now()
 );
 create index if not exists transcript_id_index on "version" (transcript_id);
 create index if not exists user_id_index on "version" (created_by_user_id);
