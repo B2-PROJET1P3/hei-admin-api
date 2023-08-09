c1c715d508fe6730214496ebe47fc4946a02d14c
Ranomenjanahary <hei.jaonary@gmail.com>
2023-08-09 11:38:55 +0300

    fix: add mock in Test_create_transcript

--- src/main/resources/db/testdata/V99_7__Test_create_transcript.sql
+++ src/main/resources/db/testdata/V99_7__Test_create_transcript.sql
@@ -2,3 +2,8 @@ insert into "transcript" (id,academic_year,creation_datetime,student,semester,is
                                                                                                  ('transcript1_id',2023,'2023-02-08T08:30:24Z','student1_id','S1',true),
                                                                                                  ('transcript2_id',2024,'2023-02-08T08:30:24Z','student2_id','S2',true);
 
+insert into "transcript"
+(id, student_id, semester, academic_year, is_definitive, creation_datetime)
+values
+    ('transcript3_id','student2_id', 'S6', 2023, true, '2023-11-11T08:25:26.00Z'),
+    ('transcript4_id','student2_id', 'S5', 2024, true, '2024-11-12T08:25:26.00Z');
