0c01d5631250e601b78a7c8a03c79eea6f606dda
Sarobidy-nantenaina <sarobidyrakotondrazafy1@gmail.com>
2023-08-09 21:36:12 +0300

    chore: creating versionIT

--- src/test/java/school/hei/haapi/integration/VersionIT.java
+++ src/test/java/school/hei/haapi/integration/VersionIT.java
@@ -1,23 +1,35 @@
 package school.hei.haapi.integration;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static school.hei.haapi.integration.conf.TestUtils.MANAGER1_TOKEN;
+import static school.hei.haapi.integration.conf.TestUtils.STUDENT1_ID;
+import static school.hei.haapi.integration.conf.TestUtils.STUDENT1_TOKEN;
+import static school.hei.haapi.integration.conf.TestUtils.TEACHER1_TOKEN;
+import static school.hei.haapi.integration.conf.TestUtils.TRANSCRIPT1_ID;
 import static school.hei.haapi.integration.conf.TestUtils.anAvailableRandomPort;
 import static school.hei.haapi.integration.conf.TestUtils.setUpCognito;
+import static school.hei.haapi.integration.conf.TestUtils.transcript1;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import school.hei.haapi.SentryConf;
+import school.hei.haapi.endpoint.rest.api.TranscriptApi;
 import school.hei.haapi.endpoint.rest.client.ApiClient;
+import school.hei.haapi.endpoint.rest.client.ApiException;
+import school.hei.haapi.endpoint.rest.model.Transcript;
 import school.hei.haapi.endpoint.rest.security.cognito.CognitoComponent;
 import school.hei.haapi.integration.conf.AbstractContextInitializer;
 import school.hei.haapi.integration.conf.TestUtils;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @Testcontainers
-@ContextConfiguration(initializers = CourseIT.ContextInitializer.class)
+@ContextConfiguration(initializers = VersionIT.ContextInitializer.class)
 @AutoConfigureMockMvc
 public class VersionIT {
 
@@ -35,7 +47,66 @@ public class VersionIT {
     setUpCognito(cognitoComponentMock);
   }
 
+  @Test
+  public void manager_read_all_ok() throws ApiException {
+    ApiClient manager1Client = anApiClient(MANAGER1_TOKEN);
+    VersionApi api = new VersionApi(manager1Client);
 
+    Transcript expected = transcript1();
+    List<Transcript> actual = api.getStudentTranscripts(STUDENT1_ID, 1,5);
+    assertEquals(expected,actual.get(0) );
+  }
+
+  @Test
+  public void teacher_read_all_ok() throws ApiException {
+    ApiClient teacher1Client = anApiClient(TEACHER1_TOKEN);
+    TranscriptApi api = new TranscriptApi(teacher1Client);
+
+    Transcript expected = transcript1();
+    List<Transcript> actual = api.getStudentTranscripts(STUDENT1_ID, 1,5);
+    assertEquals(expected,actual.get(0) );
+  }
+
+
+  @Test
+  public void student_read_own_ok() throws ApiException {
+    ApiClient student1Client = anApiClient(STUDENT1_TOKEN);
+    TranscriptApi api = new TranscriptApi(student1Client);
+
+    Transcript actual = api.getStudentTranscriptById(STUDENT1_ID, TRANSCRIPT1_ID);
+
+    assertEquals(actual, transcript1());
+  }
+
+  @Test
+  public void student_read_all_own_ok() throws ApiException {
+    ApiClient student1Client = anApiClient(STUDENT1_TOKEN);
+    TranscriptApi api = new TranscriptApi(student1Client);
+
+    List<Transcript> actual = api.getStudentTranscripts(STUDENT1_ID, 1,5);
+
+    assertEquals(actual.get(0), transcript1());
+  }
+
+  @Test
+  public void teacher_read_ok() throws ApiException {
+    ApiClient teacher1Client = anApiClient(TEACHER1_TOKEN);
+    TranscriptApi api = new TranscriptApi(teacher1Client);
+
+    Transcript actual = api.getStudentTranscriptById(STUDENT1_ID,TRANSCRIPT1_ID);
+
+    assertEquals(actual, transcript1());
+  }
+
+  @Test
+  public void manager_read_ok() throws ApiException {
+    ApiClient manager1Client = anApiClient(MANAGER1_TOKEN);
+    TranscriptApi api = new TranscriptApi(manager1Client);
+
+    Transcript actual = api.getStudentTranscriptById(STUDENT1_ID,TRANSCRIPT1_ID);
+
+    assertEquals(actual, transcript1());
+  }
 
   static class ContextInitializer extends AbstractContextInitializer {
     public static final int SERVER_PORT = anAvailableRandomPort();
