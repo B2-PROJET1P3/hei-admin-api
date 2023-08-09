853bb8c7d15abeb4bbab78fb0c805408301be099
Ranomenjanahary <hei.jaonary@gmail.com>
2023-08-09 11:36:22 +0300

    fix: add transcript integration test

--- src/test/java/school/hei/haapi/integration/TranscriptIT.java
+++ src/test/java/school/hei/haapi/integration/TranscriptIT.java
@@ -1,18 +1,19 @@
 package school.hei.haapi.integration;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static school.hei.haapi.integration.conf.TestUtils.MANAGER1_TOKEN;
 import static school.hei.haapi.integration.conf.TestUtils.STUDENT1_ID;
 import static school.hei.haapi.integration.conf.TestUtils.STUDENT1_TOKEN;
+import static school.hei.haapi.integration.conf.TestUtils.TEACHER1_TOKEN;
 import static school.hei.haapi.integration.conf.TestUtils.anAvailableRandomPort;
 import static school.hei.haapi.integration.conf.TestUtils.setUpCognito;
 import static school.hei.haapi.integration.conf.TestUtils.transcript1;
 import static school.hei.haapi.integration.conf.TestUtils.transcript2;
 
 import java.util.List;
-import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,11 +21,9 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import school.hei.haapi.SentryConf;
-import school.hei.haapi.endpoint.rest.api.TeachingApi;
 import school.hei.haapi.endpoint.rest.api.TranscriptApi;
 import school.hei.haapi.endpoint.rest.client.ApiClient;
 import school.hei.haapi.endpoint.rest.client.ApiException;
-import school.hei.haapi.endpoint.rest.model.Student;
 import school.hei.haapi.endpoint.rest.model.Transcript;
 import school.hei.haapi.endpoint.rest.security.cognito.CognitoComponent;
 import school.hei.haapi.integration.conf.AbstractContextInitializer;
@@ -62,7 +61,39 @@ void user_read_ok () throws ApiException {
 }
 
 
+  @Test
+  void student_read_ok() throws ApiException {
+    ApiClient student1Client = anApiClient(STUDENT1_TOKEN);
+    TranscriptApi api = new TranscriptApi(student1Client);
 
+    List<Transcript> actual = api.getStudentTranscripts(STUDENT1_ID, 1, 5);
+
+
+    assertTrue(actual.containsAll(List.of(transcript1(), transcript2())));
+    assertEquals(actual.get(0), transcript2());
+  }
+
+  @Test
+  void teacher_read_ok() throws ApiException {
+    ApiClient teacher1Client = anApiClient(TEACHER1_TOKEN);
+    TranscriptApi api = new TranscriptApi(teacher1Client);
+
+    List<Transcript> actual = api.getStudentTranscripts(STUDENT1_ID, 1, 5);
+
+    assertTrue(actual.containsAll(List.of(transcript1(), transcript2())));
+    assertEquals(actual.get(0), transcript2());
+  }
+
+  @Test
+  void manager_read_ok() throws ApiException {
+    ApiClient manager1Client = anApiClient(MANAGER1_TOKEN);
+    TranscriptApi api = new TranscriptApi(manager1Client);
+
+    List<Transcript> actual = api.getStudentTranscripts(STUDENT1_ID, 1, 5);
+
+    assertEquals(actual.get(1), transcript1());
+    assertEquals(actual.get(0), transcript2());
+  }
 
   static class ContextInitializer extends AbstractContextInitializer {
     public static final int SERVER_PORT = anAvailableRandomPort();
