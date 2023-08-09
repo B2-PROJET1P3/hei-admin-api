a134fd483920a386c832085a5fa73d39b633dac5
Sarobidy-nantenaina <sarobidyrakotondrazafy1@gmail.com>
2023-08-09 23:46:34 +0300

    chore: add test and data for version

--- src/main/resources/db/testdata/V99_7__Test_create_version.sql
+++ src/main/resources/db/testdata/V99_7__Test_create_version.sql
@@ -1,2 +1,2 @@
-insert into version (id , transcript_id , ref , created_by_user_id , created_by_user_role , creation_datetime) values ('version1_id','transcript1_id',1,'student1_id','2021-11-08T08:25:24.00Z'),
+insert into version (id , transcript_id , ref , created_by_user_id , creation_datetime) values ('version1_id','transcript1_id',1,'student1_id','2021-11-08T08:25:24.00Z'),
                                                                                                                       ('version2_id','transcript2_id',2,'student2_id','2022-11-08T08:25:24.00Z');
